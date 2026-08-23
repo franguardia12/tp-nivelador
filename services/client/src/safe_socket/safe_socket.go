f153e0c565cd8819d50c722219fff017e75bbfb4
Franco <faguardia@fi.uba.ar>
2026-08-22 21:45:21 -0300

    feat: enhance SendAll and RecvAll functions in client for short-read/short-write tolerance

--- services/client/src/safe_socket/safe_socket.go
+++ services/client/src/safe_socket/safe_socket.go
@@ -1,22 +1,51 @@
 package safe_socket
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
-//TODO: Complete with a short-read/short-write tolerant implementation
+func SendAll(socket io.Writer, data []byte) error {
+	totalSent := 0
 
-func SendAll(socket io.Writer, bytes []byte) error {
-	_, err := socket.Write(bytes)
-	if err != nil {
-		return err
+	for totalSent < len(data) {
+		sent, err := socket.Write(data[totalSent:])
+		totalSent += sent
+		// Retry only successful short writes. An arbitrary error may be
+		// permanent, so retrying it could hide the failure or loop forever.
+		if err != nil {
+			return err
+		}
+
 	}
+
 	return nil
 }
 
 func RecvAll(socket io.Reader, size int) ([]byte, error) {
-	buff := make([]byte, size)
-	_, err := socket.Read(buff)
-	if err != nil {
-		return nil, err
+	if size < 0 {
+		return nil, fmt.Errorf("invalid read size %d", size)
 	}
+
+	buff := make([]byte, size)
+	totalReceived := 0
+
+	for totalReceived < size {
+		received, err := socket.Read(buff[totalReceived:])
+		totalReceived += received
+		if totalReceived == size {
+			return buff, nil
+		}
+		// Keep bytes returned together with an error, but propagate that error
+		// if the requested message is still incomplete. Its cause may be permanent.
+		if err != nil {
+			if err == io.EOF {
+				return nil, io.ErrUnexpectedEOF
+			}
+			return nil, err
+		}
+
+	}
+
 	return buff, nil
 }
